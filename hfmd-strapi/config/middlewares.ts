2d3a18a27fefbbf177cc925570c0d9c59bd8614b
mkatic <michaelkatic@gmail.com>
2022-11-23 12:59:08 -0700

    Added plugins for exporting, storing images on s3. Fixed bug showing images and embeded content on editorjs. Added blog content type.

--- hfmd-strapi/config/middlewares.ts
+++ hfmd-strapi/config/middlewares.ts
@@ -1,11 +1,50 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          
+          //Required for editorjs: https://market.strapi.io/plugins/strapi-plugin-react-editorjs
+          'img-src': ['*'],
+          'frame-src': ['*'],
+          'media-src': ['*'],
+          
+          //Required for s3 provider: https://docs.strapi.io/developer-docs/latest/development/providers.html#configuring-providers
+          // 'img-src': [ 
+          //   "'self'",
+          //   'data:',
+          //   'blob:',
+          //   'dl.airtable.com',
+          //   'hfmd-strapi-prod.s3.us-west-2.amazonaws.com',
+          // ],
+          // 'media-src': [
+          //   "'self'",
+          //   'data:',
+          //   'blob:',
+          //   'dl.airtable.com',
+          //   'hfmd-strapi-prod.s3.us-west-2.amazonaws.com',
+          // ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    //https://market.strapi.io/plugins/strapi-plugin-import-export-entries#install-now-section
+    //When importing data, imported file size may exceed the file size limit of the server. To lift up the limit, configure the Strapi middleware body:
+    name: 'strapi::body',
+    config: {
+      jsonLimit: '10mb',
+    }
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
---
+++ hfmd-strapi/config/plugins.ts
@@ -0,0 +1,32 @@
+export default ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+                region: env('AWS_REGION'),
+                params: {
+                    Bucket: env('AWS_BUCKET'),
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+    'import-export-entries': {
+        enabled: true,
+        
+        /**
+         * Public hostname of the server.
+         *
+         * If you use the local provider to persist medias,
+         * `serverPublicHostname` should be set to properly export media urls.
+         */
+        serverPublicHostname: `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`, // default: "".
+    },
+});
+  
