eb0df5f84e167aa76b81c7ef16216c5632728442
mkatic <michaelkatic@gmail.com>
2022-11-25 07:03:39 -0700

    Updates to debug google auth errors. Remove import/export extension because it was causing errors.

--- hfmd-strapi/config/plugins.ts
+++ hfmd-strapi/config/plugins.ts
@@ -17,16 +17,15 @@ export default ({ env }) => ({
             },
         },
     },
-    'import-export-entries': {
-        enabled: true,
-        
-        /**
-         * Public hostname of the server.
-         *
-         * If you use the local provider to persist medias,
-         * `serverPublicHostname` should be set to properly export media urls.
-         */
-        serverPublicHostname: `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`, // default: "".
-    },
+    // 'import-export-entries': {
+    //     enabled: true,        
+    //     /**
+    //      * Public hostname of the server.
+    //      *
+    //      * If you use the local provider to persist medias,
+    //      * `serverPublicHostname` should be set to properly export media urls.
+    //      */
+    //     serverPublicHostname: `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`, // default: "".
+    // },
 });
   
